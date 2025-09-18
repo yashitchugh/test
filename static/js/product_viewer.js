4a322091a309895b267336a9babcf6b2f90a7d32
Yashit Chugh <yashitchugh00@gmail.com>
2025-09-18 23:04:29 +0530

    Add files via upload

---
+++ static/js/product_viewer.js
@@ -0,0 +1,19 @@
+renderer.domElement.addEventListener('click', function(event) {
+    const rect = renderer.domElement.getBoundingClientRect();
+    mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+
+    if (model) {
+        // Change from model.children to all descendants:
+        const meshes = [];
+        model.traverse(child => {
+            if (child.isMesh) meshes.push(child);
+        });
+        const intersects = raycaster.intersectObjects(meshes);
+        if (intersects.length > 0) {
+            selectedMesh = intersects[0].object;
+            document.getElementById('info').textContent = "Selected part: " + (selectedMesh.name || "unnamed mesh");
+        }
+    }
+});
